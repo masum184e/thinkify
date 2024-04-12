d9c68f598b02beb52a707875e4a52f3b7e78e53f
masum184e <masum184e@gmail.com>
2024-04-12 22:54:40 +0600

    baseline added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline';
+
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </React.StrictMode>,
 )
