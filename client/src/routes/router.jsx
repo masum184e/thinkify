4f882429b06e8d8d352d38b53845d69438abdee3
masum184e <masum184e@gmail.com>
2024-05-07 23:49:33 +0600

    profile, my-post, add-post added

--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -2,7 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Registration from "../pages/Registration";
-
+import Profile from "../pages/Profile";
+import MyPost from "../pages/MyPost";
+import AddPost from "../pages/AddPost";
+import UserSideBar from "../layouts/UserSideBar";
 
 const router = createBrowserRouter([
     {
@@ -14,6 +17,15 @@ const router = createBrowserRouter([
     }, {
         path: "/registration",
         element: <Registration />
+    }, {
+        path: "/profile",
+        element: <UserSideBar><Profile /></UserSideBar>,
+    }, {
+        path: "/my-post",
+        element: <UserSideBar><MyPost /></UserSideBar>
+    }, {
+        path: "/add-post",
+        element: <UserSideBar><AddPost /></UserSideBar>
     }
 ])
 
