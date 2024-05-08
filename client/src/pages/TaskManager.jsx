757d3d6fe3a85494b697c23c4ca1f486f83cb254
masum184e <masum184e@gmail.com>
2024-05-08 11:49:04 +0600

    Fab Icon Added

--- client/src/pages/TaskManager.jsx
+++ client/src/pages/TaskManager.jsx
@@ -1,6 +1,8 @@
-import { Grid, Box, List, Typography } from '@mui/material';
+import { Grid, Box, List, Typography, Fab } from '@mui/material';
 import TaskStatus from '../../components/profile/task-management/TaskStatus';
 import Task from '../../components/profile/task-management/Task';
+
+import AddIcon from '@mui/icons-material/Add';
 
 const TaskManager = () => {
     const handleDropTodo = () => {
@@ -32,53 +34,71 @@ const TaskManager = () => {
     ]
     return (
         <>
-            {/* <Typography variant="h2" textAlign="center" sx={{ mb: "16px", fontWeight: "bold" }}>Coordination of Work</Typography> */}
-            <Grid container spacing={3}>
-                <Grid item xs>
-                    <Box bgcolor="primary.main" color="primary.contrastText" p={2} sx={{ borderRadius: '5px', }}>
-                        <TaskStatus status="todo" onDrop={handleDropTodo} />
-                    </Box>
+            <Box sx={{ position: "relative" }} >
+                {/* <Typography variant="h2" textAlign="center" sx={{ mb: "16px", fontWeight: "bold" }}>Coordination of Work</Typography> */}
+                <Grid container spacing={3}>
+                    <Grid item xs>
+                        <Box bgcolor="primary.main" color="primary.contrastText" p={2} sx={{ borderRadius: '5px', }}>
+                            <TaskStatus status="todo" onDrop={handleDropTodo} />
+                        </Box>
+                    </Grid>
+                    <Grid item xs>
+                        <Box bgcolor="secondary.main" color="secondary.contrastText" p={2} sx={{ borderRadius: '5px', }}>
+                            <TaskStatus status="ongoing" onDrop={handleDropOngoing} />
+                        </Box>
+                    </Grid>
+                    <Grid item xs>
+                        <Box bgcolor="warning.main" color="warning.contrastText" p={2} sx={{ borderRadius: '5px', }}>
+                            <TaskStatus status="completed" onDrop={handleDropCompleted} />
+                        </Box>
+                    </Grid>
                 </Grid>
-                <Grid item xs>
-                    <Box bgcolor="secondary.main" color="secondary.contrastText" p={2} sx={{ borderRadius: '5px', }}>
-                        <TaskStatus status="ongoing" onDrop={handleDropOngoing} />
-                    </Box>
+                <Grid container sx={{ minHeight: "70vh" }} spacing={3}>
+                    <Grid item xs>
+                        <List>
+                            {
+                                todo.map((item) => (
+                                    <Task key={item._id} text={item.title} taskId={item._id} />
+                                ))
+                            }
+                        </List>
+                    </Grid>
+                    <Grid item xs>
+                        <List>
+                            {
+                                ongoing.map((item) => (
+                                    <Task key={item._id} text={item.title} taskId={item._id} />
+                                ))
+                            }
+                        </List>
+                    </Grid>
+                    <Grid item xs>
+                        <List>
+                            {
+                                completed.map((item) => (
+                                    <Task key={item._id} text={item.title} taskId={item._id} />
+                                ))
+                            }
+                        </List>
+                    </Grid>
                 </Grid>
-                <Grid item xs>
-                    <Box bgcolor="warning.main" color="warning.contrastText" p={2} sx={{ borderRadius: '5px', }}>
-                        <TaskStatus status="completed" onDrop={handleDropCompleted} />
-                    </Box>
-                </Grid>
-            </Grid>
-            <Grid container sx={{ minHeight: "70vh" }} spacing={3}>
-                <Grid item xs>
-                    <List>
-                        {
-                            todo.map((item) => (
-                                <Task key={item._id} text={item.title} taskId={item._id} />
-                            ))
-                        }
-                    </List>
-                </Grid>
-                <Grid item xs>
-                    <List>
-                        {
-                            ongoing.map((item) => (
-                                <Task key={item._id} text={item.title} taskId={item._id} />
-                            ))
-                        }
-                    </List>
-                </Grid>
-                <Grid item xs>
-                    <List>
-                        {
-                            completed.map((item) => (
-                                <Task key={item._id} text={item.title} taskId={item._id} />
-                            ))
-                        }
-                    </List>
-                </Grid>
-            </Grid>
+
+
+                <Fab
+                    aria-label="add"
+                    sx={{
+                        position: 'absolute',
+                        bottom: '50px',
+                        right: '70px',
+                        borderRadius: '50%',
+                        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+                        backgroundColor: "#59e3a7",
+                        color: "white"
+                    }}
+                >
+                    <AddIcon />
+                </Fab>
+            </Box>
         </>
     )
 }
