1a0a3319fff7764d30b018a586cc3b9cddc478ff
masum184e <masum184e@gmail.com>
2024-05-08 20:43:00 +0600

    card added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,54 +1,78 @@
 import { Box, Grid, Avatar, Typography, Card, Button } from '@mui/material';
+import NotesIcon from '@mui/icons-material/Notes';
 import { Link } from 'react-router-dom';
 
 import VerifiedIcon from '@mui/icons-material/Verified';
 
 const Profile = () => {
     return (
-        <Grid container m={2}>
-            <Grid item xs="4">1</Grid>
-            <Grid item xs="4">2</Grid>
-            <Grid item xs="4" >
-                <Box sx={{ position: "relative", marginTop: "50px" }}>
-                    <Box sx={{
-                        margin: "auto",
-                        height: "90px",
-                        width: "90px",
-                        padding: "5px",
-                        backgroundColor: "#59e3a7",
-                        borderRadius: "50%",
-                        border: "2px solid lightgray",
-                        position: "absolute",
-                        zIndex: "50",
-                        left: "50%",
-                        transform: "translateX(-50%)",
-                        top: "-50px"
-                    }}>
-                        <Avatar sx={{ width: "100%", height: "100%" }} alt="Hdy Baker" src="https://cdn-icons-png.flaticon.com/512/5556/5556468.png" />
-                    </Box>
-                    <Card sx={{ backgroundColor: "#59e3a7", borderRadius: "10px", padding: "40px 15px 15px 15px" }}  >
-                        <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                            <Typography variant="h1"
-                                sx={{
-                                    fontSize: "45px",
-                                    fontWeight: "bold"
-                                }}
-                            >Masum Billah</Typography>
-                            <VerifiedIcon />
+        <Grid container  >
+            <Grid item xs="8" sx={{ paddingRight: "15px" }}>
+                <Box sx={{ display: "flex", justifyContent: "space-between", gap: "10px" }}>
+                    <Box sx={{ backgroundColor: "#59e3a7", padding: "5px 10px 15px 5px", borderRadius: "5px", flex: "1", boxShadow: "0px 0px 3px 0px #1b2e35" }}>
+                        <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "space-between" }}>
+                            <NotesIcon sx={{ color: "white" }} />
+                            <Typography variant="span" sx={{ color: "white" }} >Total Post</Typography>
                         </Box>
-                        <Typography variant="h1"
-                            sx={{
-                                fontSize: "20px"
-                            }}
-                        >masum184e@gmail.com</Typography>
-                        <Typography sx={{ color: "gray", margin: "5px 0" }} variant="body1">At Thinkify, our mission is to provide a dynamic and intuitive platform that empowers individuals to transform their ideas into actionable tasks.</Typography>
-                        <Button component={Link} to="update-profile" sx={{
-                            backgroundColor: "white",
-                            color: "black",
-                            "&:hover": {
-                                backgroundColor: "white"
-                            }
-                        }} fullWidth >Edit Profile</Button>
+                        <Box sx={{ width: "60px", height: "60px", padding: "15px", fontSize: "20px", fontWeight: "semibold", backgroundColor: "white", borderRadius: "50%", textAlign: "center", margin: "10px auto 0 auto" }}>
+                            <Typography variant="span" >45</Typography>
+                        </Box>
+                    </Box>
+                    <Box sx={{ backgroundColor: "#59e3a7", padding: "5px 10px 15px 5px", borderRadius: "5px", flex: "1", boxShadow: "0px 0px 3px 0px #1b2e35" }}>
+                        <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "space-between" }}>
+                            <NotesIcon sx={{ color: "white" }} />
+                            <Typography variant="span" sx={{ color: "white" }} >Total Assignments</Typography>
+                        </Box>
+                        <Box sx={{ width: "60px", height: "60px", padding: "15px", fontSize: "20px", fontWeight: "semibold", backgroundColor: "white", borderRadius: "50%", textAlign: "center", margin: "10px auto 0 auto" }}>
+                            <Typography variant="span" >45</Typography>
+                        </Box>
+                    </Box>
+                    <Box sx={{ backgroundColor: "#59e3a7", padding: "5px 10px 15px 5px", borderRadius: "5px", flex: "1", boxShadow: "0px 0px 3px 0px #1b2e35" }}>
+                        <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "space-between" }}>
+                            <NotesIcon sx={{ color: "white" }} />
+                            <Typography variant="span" sx={{ color: "white" }} >Total Product</Typography>
+                        </Box>
+                        <Box sx={{ width: "60px", height: "60px", padding: "15px", fontSize: "20px", fontWeight: "semibold", backgroundColor: "white", borderRadius: "50%", textAlign: "center", margin: "10px auto 0 auto" }}>
+                            <Typography variant="span" >45</Typography>
+                        </Box>
+                    </Box>
+                </Box>
+            </Grid>
+            <Grid item xs="4" >
+                <Box >
+                    <Card sx={{
+                        backgroundColor: "#59e3a7",
+                        borderRadius: "4px",
+                        padding: "15px 15px 15px 15px",
+                        boxShadow: "0px 0px 3px 0px #1b2e35"
+                    }}  >
+                        <Box sx={{ display: "flex", justifyContent: "space-between", color: "white" }}>
+                            <Box>
+                                <Typography variant="h1"
+                                    sx={{
+                                        fontSize: "45px",
+                                        fontWeight: "bold"
+                                    }}
+                                >Masum Billah</Typography>
+                                <Typography variant="h1"
+                                    sx={{
+                                        fontSize: "20px"
+                                    }}
+                                >masum184e@gmail.com</Typography>
+                            </Box>
+                            <Box sx={{
+                                margin: "auto",
+                                height: "85px",
+                                width: "85px",
+                                padding: "5px",
+                                backgroundColor: "#59e3a7",
+                                borderRadius: "50%",
+                                border: "2px solid white",
+                            }}>
+                                <Avatar sx={{ width: "100%", height: "100%" }} alt="Hdy Baker" src="https://cdn-icons-png.flaticon.com/512/5556/5556468.png" />
+                            </Box>
+                        </Box>
+                        {/* <Typography sx={{ color: "gray", margin: "5px 0" }} variant="body1">At Thinkify, our mission is to provide a dynamic and intuitive platform that empowers individuals to transform their ideas into actionable tasks.</Typography> */}
                     </Card>
                 </Box>
             </Grid>
