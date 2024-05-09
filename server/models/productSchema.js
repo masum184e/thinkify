db791b931113773ceb978496d21fd526e34441fc
masum184e <masum184e@gmail.com>
2024-05-09 06:31:57 +0600

    product schema added

---
+++ server/models/productSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    authorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
