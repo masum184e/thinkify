cffd3829db320abd878f0e8710d8bbc935d71a87
masum184e <masum184e@gmail.com>
2024-05-09 06:30:51 +0600

    task schema added

---
+++ server/models/taskSchema.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    deadline: {
+        type: Date,
+        required: true,
+    },
+    priority: {
+        type: String,
+        enum: ['Low', 'Moderate', 'High', 'Critical'],
+        required: true,
+    },
+    authorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const TaskModel = mongoose.model('Task', taskSchema);
+
+export default TaskModel;
