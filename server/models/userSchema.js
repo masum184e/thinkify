819e404fc0895fdadf7f4f7db436fa26828851a9
masum184e <masum184e@gmail.com>
2024-05-09 06:27:06 +0600

    userschema added

---
+++ server/models/userSchema.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    default: null
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User
