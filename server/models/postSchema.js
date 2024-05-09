b58d6377b6656ffb682109191bb73db63e111e6f
masum184e <masum184e@gmail.com>
2024-05-09 06:26:58 +0600

    createdat added

--- server/models/postSchema.js
+++ server/models/postSchema.js
@@ -18,6 +18,10 @@ const postSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 });
 
