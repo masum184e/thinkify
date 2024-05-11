d0f6f098b05568e6fae7583d22810425659e5c1d
masum184e <masum184e@gmail.com>
2024-05-11 22:22:36 +0600

    auth middleware added

---
+++ server/middleware/userAuthentication.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+import { Types } from 'mongoose';
+
+import UserModel from "../models/userSchema.js";
+
+const userAuthentication = async (req, res, next) => {
+    try {
+        const cookieKey = process.env.EXPRESSJS_COMPLETE_AUTHENTICATION_TOKEN_COOKIE_KEY
+        const authorizationToken = req.cookies[cookieKey];
+        if (authorizationToken) {
+
+            const { userId } = jwt.verify(authorizationToken, process.env.JWT_SECRET_KEY)
+            if (Types.ObjectId.isValid(userId)) {
+
+                const user = await UserModel.findById(userId).select("-password");
+                console.log(user)
+                if (user.role === "user") {
+                    req.user = user
+                    next();
+                } else {
+                    return res.status(403).json({ "status": false, "message": "Invalid Request" });
+                }
+
+            } else {
+                return res.status(403).json({ "status": false, "message": "Invalid Request" });
+            }
+
+        } else {
+            res.status(401).json({ "status": false, "message": "Authorization Failed" });
+        }
+
+    } catch (error) {
+        res.status(500).json({ "status": false, "message": "Internal Server Error", "error": error });
+    }
+}
+
+export default userAuthentication;
