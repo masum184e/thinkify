8562ef0ef929534c2c284b9efbcdf67e08771422
masum184e <masum184e@gmail.com>
2024-05-11 22:22:04 +0600

    user auth middleware updated

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getUserData, logOut, login, registration } from '../controller/user.js';
+import userAuthentication from '../middleware/userAuthentication.js';
 
 const user = express.Router();
 
-user.post("/registration",registration);
-user.post("/login",login);
-user.get("/profile",getUserData);
-user.get("/log-out",logOut);
+user.post("/registration", registration);
+user.post("/login", login);
+user.get("/profile", userAuthentication, getUserData);
+user.get("/log-out", logOut);
 
 export default user;
