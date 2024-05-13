d419cdf59cc79738e6b5935a7f80656d47322f8d
masum184e <masum184e@gmail.com>
2024-05-13 09:39:08 +0600

    context provider added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/router.jsx';
+import Provider from '../provider/Provider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssBaseline>
-      <RouterProvider router={router} />
+      <Provider>
+        <RouterProvider router={router} />
+      </Provider>
     </CssBaseline>
   </React.StrictMode>,
 )
