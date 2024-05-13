65b5c3e1648ea878c430d073eb25f2b393c3a0bc
masum184e <masum184e@gmail.com>
2024-05-13 11:34:01 +0600

    login added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,52 @@
 import { Box, Typography, TextField, FormControlLabel, Checkbox, Button, Divider } from "@mui/material"
 import { Link, useNavigate } from "react-router-dom";
 import GoogleIcon from '@mui/icons-material/Google';
+import { useForm } from "react-hook-form";
+import Cookies from 'js-cookie'
+import { useEffect } from "react";
 
 const Login = () => {
     const navigate = useNavigate();
-    const handleSubmit = e => {
-        e.preventDefault();
-        navigate("/profile")
-    }
+    const { alertMessage, alertBoxOpenStatus, setAlertBoxOpenStatus, setAlertMessage, setAlertSeverity } = useThinkify();
+    const { login, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+    });
+    const onSubmit = async (data) => {
+        try {
+            const response = await axios({
+                baseURL: import.meta.env.VITE_SERVER_ENDPOINT,
+                url: "/users/login",
+                withCredentials: true,
+                method: "POST",
+                data
+            });
+            if (response.data.status) {
+                navigate("/profile")
+                console.log(response.data.message);
+            } else {
+                setAlertBoxOpenStatus(true);
+                setAlertSeverity("error");
+                setAlertMessage(response.data.message)
+                console.log(alertBoxOpenStatus, alertMessage)
+            }
+        } catch (error) {
+            console.log(error);
+            setAlertBoxOpenStatus(true);
+            setAlertSeverity("error");
+            setAlertMessage(error.message)
+            console.log(alertBoxOpenStatus, alertMessage)
+        }
+    };
+    useEffect(() => {
+        const cookie = Cookies.get(import.meta.env.VITE_COOKIE_KEY)
+        if (cookie) {
+            navigate("/profile")
+        }
+    }, [navigate]);
+
     return (
         <>
             <Box height="100vh" sx={{ display: "flex" }}>
@@ -24,12 +63,13 @@ const Login = () => {
                 <Box sx={{ flex: 1, backgroundColor: "#1b2e35", display: "flex", alignItems: "center" }}>
                     <Box width={1 / 2} mx="auto" my="auto" >
                         <Typography variant="h2" component="h2" sx={{ color: "white", fontSize: "2.25rem", fontWeight: "bold" }}>Welcome Back</Typography>
-                        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 4 }}>
+                        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ mt: 4 }}>
                             <TextField
                                 fullWidth
                                 placeholder="Enter Email"
                                 sx={{
-                                    mb: 2,
+                                    mb: 1,
+                                    color: "white",
                                     "& .MuiOutlinedInput-root": {
                                         "& fieldset": {
                                             borderColor: "white",
@@ -47,11 +87,16 @@ const Login = () => {
                                         },
                                     },
                                 }}
+                                {...login("email", { required: true })}
                             />
+                            {errors.email && <Typography variant="p" component="p" sx={{ color: "red", mb: 2 }}>{errors.email.message}</Typography>}
                             <TextField
                                 fullWidth
                                 placeholder="Enter Password"
+                                type="password"
                                 sx={{
+                                    mb: 1,
+                                    color: "white",
                                     "& .MuiOutlinedInput-root": {
                                         "& fieldset": {
                                             borderColor: "white",
@@ -69,7 +114,9 @@ const Login = () => {
                                         },
                                     },
                                 }}
+                                {...login("password", { required: true })}
                             />
+                            {errors.password && <Typography variant="p" component="p" sx={{ color: "red" }}>{errors.password.message}</Typography>}
                             <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                                 <FormControlLabel
                                     control={<Checkbox color="primary" />}
