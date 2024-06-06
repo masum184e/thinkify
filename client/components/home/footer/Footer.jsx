251e62448a81fb55aa30c8855d7dd014a8a45b98
masum184e <masum184e@gmail.com>
2024-06-06 23:04:57 +0600

    footer updated

--- client/components/home/footer/Footer.jsx
+++ client/components/home/footer/Footer.jsx
@@ -1,8 +1,15 @@
-import { Grid, Typography, Box, Divider, Button } from '@mui/material';
+import { Grid, Typography, Box, Divider, Button } from "@mui/material";
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: '#1b2e35', color: 'white', paddingTop: '4rem', paddingBottom: '1rem' }}>
+    <footer
+      style={{
+        backgroundColor: "#1b2e35",
+        color: "white",
+        paddingTop: "4rem",
+        paddingBottom: "1rem",
+      }}
+    >
       <Box maxWidth="1280px" mx="auto">
         <Grid container>
           <Grid item>
@@ -11,66 +18,83 @@ const Footer = () => {
               <Typography
                 sx={{
                   fontFamily: "Platypi",
-                  color: "#59e3a7"
+                  color: "#59e3a7",
                 }}
                 variant="h3"
                 component="h3"
-              >Thinkify</Typography>
+              >
+                Thinkify
+              </Typography>
             </Box>
-            <Typography variant="body1" >Connecting Ideas, Inspiring Perspectives</Typography>
+            <Typography variant="body1">
+              Connecting Ideas, Inspiring Perspectives
+            </Typography>
           </Grid>
         </Grid>
-        <Grid container my={6}>
-          <Grid item>
-            <Typography variant="subtitle1" fontWeight="bold">Solutions</Typography>
-            <ul style={{ listStyleType: 'none', padding: 0 }}>
-              <li>Control</li>
-              <li>Scalability & Efficiency</li>
-              <li>Supported by Experts</li>
-              <li>Performance & Security</li>
+        <Grid container my={6} >
+          <Grid item xs={2}>
+            <Typography variant="subtitle1" fontWeight="bold">
+              Solutions
+            </Typography>
+            <ul style={{ listStyleType: "none", padding: 0 }}>
+              <li>Facebook</li>
+              <li>Twitter</li>
+              <li>Linkedin</li>
             </ul>
           </Grid>
-          <Grid item>
-            <Typography variant="subtitle1" fontWeight="bold">Products</Typography>
-            <ul style={{ listStyleType: 'none', padding: 0 }}>
-              <li>Integrated Platform</li>
-              <li>Payroll</li>
-              <li>Human Resources</li>
-              <li>Workforce Management</li>
-              <li>Recruiting & Onboarding</li>
+          <Grid item xs={2}>
+            <Typography variant="subtitle1" fontWeight="bold">
+              Products
+            </Typography>
+            <ul style={{ listStyleType: "none", padding: 0 }}>
+              <li>Community</li>
+              <li>Forums</li>
             </ul>
           </Grid>
-          <Grid item>
-            <Typography variant="subtitle1" fontWeight="bold">Resources</Typography>
-            <ul style={{ listStyleType: 'none', padding: 0 }}>
+          <Grid item xs={2}>
+            <Typography variant="subtitle1" fontWeight="bold">
+              Resources
+            </Typography>
+            <ul style={{ listStyleType: "none", padding: 0 }}>
               <li>Case Studies</li>
               <li>Blog</li>
             </ul>
           </Grid>
-          <Grid item>
-            <Typography variant="subtitle1" fontWeight="bold">Company</Typography>
-            <ul style={{ listStyleType: 'none', padding: 0 }}>
+          <Grid item xs={2}>
+            <Typography variant="subtitle1" fontWeight="bold">
+              Company
+            </Typography>
+            <ul style={{ listStyleType: "none", padding: 0 }}>
               <li>About Us</li>
               <li>Careers</li>
-              <li>Newsroom</li>
               <li>Contact Us</li>
             </ul>
           </Grid>
-          <Grid item>
-            <Box>
+          <Grid item xs={4}>
+            <Box
+              sx={{
+                background: "gray",
+                textAlign: "right",
+                borderRadius:"2px",
+                borderTopLeftRadius: "50px",
+                padding: "10px",
+                backgroundColor:"#59e3a7",
+                color:"#1b2e35"
+              }}
+            >
               <Typography>1-800-600-0464</Typography>
-              <Typography fontWeight="bold" color="#000dff">support@thinkify.com</Typography>
+              <Typography>support@thinkify.com</Typography>
               <Typography>900-140 10th Avenue SE</Typography>
               <Typography>Calgary, AB TG 0R1</Typography>
             </Box>
           </Grid>
         </Grid>
-        <Divider style={{ marginBottom: '1rem' }} />
+        <Divider style={{ marginBottom: "1rem" }} />
         <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Typography variant="body2">Copyright 2023 ZenTalk. All rights reserved</Typography>
+          <Typography variant="body2">Copyright {new Date().getFullYear()} Thikify. All rights reserved</Typography>
           <Box>
-            <Button sx={{ color: "#59e3a7" }}>Privacy Policy</Button>
-            <Button sx={{ color: "#59e3a7" }}>Terms of Services</Button>
+            <Button sx={{ color: "#59e3a7", "&:hover": { backgroundColor: "transparent" } }}>Privacy Policy</Button>
+            <Button sx={{ color: "#59e3a7", "&:hover": { backgroundColor: "transparent" } }}>Terms of Services</Button>
           </Box>
         </Box>
       </Box>
