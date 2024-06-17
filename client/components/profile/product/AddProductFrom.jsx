c1fcb6883f57d0c44eab46b975ee7010cc1d399f
masum184e <masum184e@gmail.com>
2024-06-17 13:33:28 +0600

    updated

--- client/components/profile/product/AddProductFrom.jsx
+++ client/components/profile/product/AddProductFrom.jsx
@@ -7,93 +7,101 @@ import { useForm } from "react-hook-form";
 const AddProductFrom = () => {
   const { register, handleSubmit, setValue, watch } = useForm();
   const file = watch("productimage");
+  const onSubmit = (data) => {
+    const formData = new FormData();
+    formData.append("file", data.file[0]);
+    console.log(formData);
+  };
   return (
     <>
-      <Box>
-        <TextField
-          label="Product Title"
-          placeholder="Enter Product Title"
-          fullWidth
-          sx={{ marginBottom: 1 }}
-        />
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Box>
+          <TextField
+            label="Product Title"
+            placeholder="Enter Product Title"
+            fullWidth
+            sx={{ marginBottom: 1 }}
+          />
+          <Box>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#59e3a7",
+                marginBottom: 1,
+              }}
+              fullWidth
+              onClick={() => {
+                document.getElementById("productimage").click();
+              }}
+            >
+              Upload Images
+            </Button>
+            <input
+              id="productimage"
+              style={{ display: "none" }}
+              type="file"
+              {...register("productimage", {
+                required: "File is required",
+                validate: {
+                  validFileType: (value) =>
+                    value &&
+                    ["image/jpeg", "image/png"].includes(value[0].type),
+                },
+              })}
+            />
+          </Box>
+          <Box>
+            {file && (
+              <>
+                <Box sx={{ position: "relative", display: "inline" }}>
+                  <img
+                    src={URL.createObjectURL(file[0])}
+                    alt=""
+                    width="100"
+                    height="50"
+                    style={{ border: "1px solid #59e3a7", borderRadius: "5px" }}
+                  />
+                  <IconButton
+                    aria-label="delete"
+                    sx={{
+                      position: "absolute",
+                      right: "-10px",
+                      zIndex: "500",
+                      bottom: "0",
+                      color: "red",
+                    }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Box>
+              </>
+            )}
+          </Box>
+
+          <TextField
+            label="Product Price"
+            placeholder="Enter Product Price"
+            fullWidth
+          />
+
+          <TextField
+            label="Description"
+            multiline
+            rows={4}
+            variant="outlined"
+            fullWidth
+            sx={{ marginY: 1 }}
+          />
+
           <Button
             variant="contained"
-            sx={{
-              backgroundColor: "#59e3a7",
-              marginBottom: 1,
-            }}
             fullWidth
-            onClick={() => {
-              document.getElementById("productimage").click();
-            }}
+            sx={{ backgroundColor: "#59e3a7" }}
           >
-            Upload Images
+            Add
           </Button>
-          <input
-            id="productimage"
-            style={{ display: "none" }}
-            type="file"
-            {...register("productimage", {
-              required: "File is required",
-              validate: {
-                validFileType: (value) =>
-                  value && ["image/jpeg", "image/png"].includes(value[0].type),
-              },
-            })}
-          />
         </Box>
-        <Box>
-          {file && (
-            <>
-              <Box sx={{ position: "relative", display: "inline" }}>
-                <img
-                  src={URL.createObjectURL(file[0])}
-                  alt=""
-                  width="100"
-                  height="50"
-                  style={{ border: "1px solid #59e3a7", borderRadius: "5px" }}
-                />
-                <IconButton
-                  aria-label="delete"
-                  sx={{
-                    position: "absolute",
-                    right: "-10px",
-                    zIndex: "500",
-                    bottom: "0",
-                    color: "red",
-                  }}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </Box>
-            </>
-          )}
-        </Box>
-
-        <TextField
-          label="Product Price"
-          placeholder="Enter Product Price"
-          fullWidth
-        />
-
-        <TextField
-          label="Description"
-          multiline
-          rows={4}
-          variant="outlined"
-          fullWidth
-          sx={{ marginY: 1 }}
-        />
-
-        <Button
-          variant="contained"
-          fullWidth
-          sx={{ backgroundColor: "#59e3a7" }}
-        >
-          Add
-        </Button>
-      </Box>
+      </form>
     </>
   );
 };
