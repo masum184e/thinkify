ddc5ef7eff8790cb421fe153be661c1c95f30f87
masum184e <masum184e@gmail.com>
2024-06-17 12:20:06 +0600

    SimpleMdeReact added

--- client/src/pages/AddPost.jsx
+++ client/src/pages/AddPost.jsx
@@ -2,10 +2,14 @@ import { Box, TextField, Button, InputBase, Chip } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 
+import SimpleMdeReact from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
 const AddPost = () => {
   const { handleSubmit, register } = useForm();
   const [tag, setTag] = useState("");
   const [tags, setTags] = useState([]);
+  const [description, setDescription] = useState("");
   const handleKeyDown = (event) => {
     if (event.key === "Enter" && tag.trim() !== "") {
       event.preventDefault();
@@ -18,7 +22,7 @@ const AddPost = () => {
     setTags(newTags);
   };
   const onSubmit = (data) => {
-    console.log(data);
+    console.log(data, tags, description);
   };
   return (
     <>
@@ -78,8 +82,35 @@ const AddPost = () => {
                 />
               </Box>
             </Box>
-            <Box sx={{ flex: "1" }}></Box>
+            <Box sx={{ flex: "1" }}>
+              <Box>
+                <label
+                  htmlFor=""
+                  style={{ fontSize: "25px", fontWeight: "bold" }}
+                >
+                  Title
+                </label>
+                <SimpleMdeReact
+                  id="description"
+                  value={description}
+                  onChange={setDescription}
+                />
+              </Box>
+            </Box>
           </Box>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 2,
+              color: "white",
+              backgroundColor: "#1b2e35",
+              "&:hover": { backgroundColor: "#1b2e35" },
+            }}
+          >
+            Post
+          </Button>
         </form>
       </Box>
     </>
