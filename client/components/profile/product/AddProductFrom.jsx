d8be4ef45638f489793d7beb2b0534eb2c572b50
masum184e <masum184e@gmail.com>
2024-06-17 13:32:13 +0600

    ile upload and visualize added

--- client/components/profile/product/AddProductFrom.jsx
+++ client/components/profile/product/AddProductFrom.jsx
@@ -1,52 +1,101 @@
 import { Box, Button, TextField, IconButton } from "@mui/material";
 
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import { useForm } from "react-hook-form";
 
 const AddProductFrom = () => {
-    return (
-        <>
-            <Box>
-                <TextField
-                    label="Product Title"
-                    placeholder="Enter Product Title"
-                    fullWidth
-                    sx={{ marginBottom: 1 }}
+  const { register, handleSubmit, setValue, watch } = useForm();
+  const file = watch("productimage");
+  return (
+    <>
+      <Box>
+        <TextField
+          label="Product Title"
+          placeholder="Enter Product Title"
+          fullWidth
+          sx={{ marginBottom: 1 }}
+        />
+        <Box>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "#59e3a7",
+              marginBottom: 1,
+            }}
+            fullWidth
+            onClick={() => {
+              document.getElementById("productimage").click();
+            }}
+          >
+            Upload Images
+          </Button>
+          <input
+            id="productimage"
+            style={{ display: "none" }}
+            type="file"
+            {...register("productimage", {
+              required: "File is required",
+              validate: {
+                validFileType: (value) =>
+                  value && ["image/jpeg", "image/png"].includes(value[0].type),
+              },
+            })}
+          />
+        </Box>
+        <Box>
+          {file && (
+            <>
+              <Box sx={{ position: "relative", display: "inline" }}>
+                <img
+                  src={URL.createObjectURL(file[0])}
+                  alt=""
+                  width="100"
+                  height="50"
+                  style={{ border: "1px solid #59e3a7", borderRadius: "5px" }}
                 />
-                <Button variant="contained"
-                    sx={{
-                        backgroundColor: "#59e3a7",
-                        marginBottom: 1
-                    }}
-                    fullWidth >Upload Images</Button>
-                <Box>
-                    <Box sx={{ position: "relative", display: "inline" }}>
-                        <img src="/images/banner.jpg" alt="" width="100" style={{ border: "1px solid #59e3a7", borderRadius: "5px" }} />
-                        <IconButton aria-label="delete" sx={{ position: "absolute", right: "-10px", zIndex: "500", bottom: "0", color: "red" }} >
-                            <DeleteIcon />
-                        </IconButton>
-                    </Box>
-                </Box>
+                <IconButton
+                  aria-label="delete"
+                  sx={{
+                    position: "absolute",
+                    right: "-10px",
+                    zIndex: "500",
+                    bottom: "0",
+                    color: "red",
+                  }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </Box>
+            </>
+          )}
+        </Box>
 
-                <TextField
-                    label="Product Price"
-                    placeholder="Enter Product Price"
-                    fullWidth
-                />
+        <TextField
+          label="Product Price"
+          placeholder="Enter Product Price"
+          fullWidth
+        />
 
-                <TextField
-                    label="Description"
-                    multiline
-                    rows={4}
-                    variant="outlined"
-                    fullWidth
-                    sx={{ marginY: 1 }}
+        <TextField
+          label="Description"
+          multiline
+          rows={4}
+          variant="outlined"
+          fullWidth
+          sx={{ marginY: 1 }}
+        />
 
-                />
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{ backgroundColor: "#59e3a7" }}
+        >
+          Add
+        </Button>
+      </Box>
+    </>
+  );
+};
 
-                <Button variant="contained" fullWidth sx={{ backgroundColor: "#59e3a7" }} >Add</Button>
-            </Box>
-        </>
-    )
-}
-
-export default AddProductFrom
+export default AddProductFrom;
