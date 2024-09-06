f7df4b4a2df5fe2f81395dded40e9cf2ca710048
masum184e <masum184e@gmail.com>
2024-09-06 23:33:45 +0600

    Test Added

---
+++ client/components/home/features/Test.jsx
@@ -0,0 +1,54 @@
+import { Box, Grid, Typography } from "@mui/material";
+
+const Thoughts = () => {
+  return (
+    <Grid
+      container
+      sx={{
+        maxWidth: "1280px",
+        mx: "auto",
+        alignItems: "center",
+      }}
+    >
+      <Grid item xs={12} md={6} container>
+        <Box
+          sx={{
+            marginLeft: "auto",
+            textAlign: "left",
+          }}
+        >
+          <Typography
+            variant="h4"
+            component="h1"
+            gutterBottom
+            sx={{ fontWeight: "bold", color: "#1b2e35" }}
+          >
+            Practice Exams
+          </Typography>
+          <Typography variant="body1" color="#797979" marginTop="-10px">
+            Test your knowledge anytime, anywhere.
+          </Typography>
+          <Typography variant="body1" color="#797979" marginTop="80px">
+            Take advantage of our comprehensive online test system to assess
+            your knowledge and track your progress. With a variety of practice
+            exams and quizzes tailored to your curriculum, you can improve your
+            skills and boost your confidence before the big test.
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <img
+          style={{
+            width: "100%",
+            borderRadius: "8px",
+            objectFit: "cover",
+          }}
+          src="/images/test.jpg"
+          alt="Practice Exams"
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Thoughts;
