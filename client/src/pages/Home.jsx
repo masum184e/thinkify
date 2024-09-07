00b576fbc0fa3bcffc2c60a373a82b7d86b50502
masum184e <masum184e@gmail.com>
2024-09-07 12:53:11 +0600

    updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,8 +22,8 @@ const Home = () => {
       <NavBar />
       <Banner />
       <Features />
-      <Membership />
       <Newsletter />
+      <Membership />
       <FAQ />
       <Footer />
     </>
